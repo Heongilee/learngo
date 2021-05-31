897743171afb6ab4389dcb530c32c51df3ba798b
Heongilee <gjsrl1@gmail.com>
2021-05-31 23:33:23 +0900

    210531 23:33

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Heongilee/learngo
+
+go 1.16
--- main.go
+++ main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Heongilee/learngo/something"
 )
 
-// fmt package
-//
-// 포맷팅을 위한 패키지.
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
 
-// Go에서는 너가 어떤 패키지를 사용하는지 작성해줘야 해
-// 다른 오픈소스 프로젝트에 main.go가 없다는 것은 오픈 소스 프로젝트 개발자가
-// 그것을 컴파일하기 원치 않기 때문이라고 볼 수 있지.
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
-// 왜 하필 함수 이름 첫 글자가 대문자(Uppercase)일까?
-//
-// Go에서는 function을 export 시킬때 대문자로 시작해주면 되기 때문이다.
 func main() {
-	fmt.Println("Hello world!")
+	// totalLength, upperName := lenAndUpper("heongi")
 
-	something.SayHello()
+	repeatMe("nico", "heongi", "dal", "marl", "flynn")
 }

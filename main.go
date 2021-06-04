d5355be05baaeddf3483793fd5cdee0d54fc5a8d
Heongilee <gjsrl1@gmail.com>
2021-06-04 17:07:28 +0900

    210604 17:07

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// func lenAndUpper(name string) (int, string) {
-// 	return len(name), strings.ToUpper(name)
-// }
+func superAdd(numbers ...int) (cnt int) {
+	// 방법 1
+	for idx, number := range numbers {
+		fmt.Println("numbers[", idx, "] =", number)
+		cnt += number
+	}
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+	// 방법 2
+	/*
+		defer fmt.Println()
+
+		for _, number := range numbers {
+			fmt.Print(number, " ")
+			cnt += number
+		}
+	*/
+
+	// 방법 3
+	/*
+		for i := 0; i < len(numbers); i++ {
+			fmt.Println("numbers[", i, "] =", numbers[i])
+			cnt += numbers[i]
+		}
+	*/
+	return
 }
 
 func main() {
-	// totalLength, upperName := lenAndUpper("heongi")
-
-	repeatMe("nico", "heongi", "dal", "marl", "flynn")
+	total := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println("result is", total)
 }
